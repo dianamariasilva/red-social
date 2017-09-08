ca8d56ad831732d8aaf92fc8e523255ea1cd4be6
dianamariasilva <diana.silva.v94@gmail.com>
2017-09-08 11:57:07 -0500

    recargar pagina y siguen comentarios

--- js/main.js
+++ js/main.js
@@ -1,36 +1,47 @@
 /*aquí va tu código*/
 
+mostrarComentarios();
+
 function guardarDatos(){
-	localStorage.clave = document.getElementById('clave').value;
-	localStorage.valor = document.getElementById('valor').value;
+	
+	localStorage.setItem ( document.getElementById('clave').value, 
+							document.getElementById('valor').value );
+
 }
 
 function guardar(){
 	var jsonData = {};
 
 	guardarDatos();
-	if ((localStorage.clave != undefined) && (localStorage.valor != undefined)){
-		 	// document.getElementById("clave").innerHTML = '';
-		 	// document.getElementById("valor").innerHTML = '';
-		 	var div = document.createElement('DIV');
-	 		div.setAttribute("class", "comentarios");
-	 		var div1 = document.createElement('DIV');
-	 		div1.setAttribute("class", "div1");
-	 		var div2 = document.createElement('DIV');
-	 		div2.setAttribute("class", "div2");
-	 		var datos = document.getElementById('datos');
-	 		var text= document.createTextNode(localStorage.clave);
-	 		var text2= document.createTextNode(localStorage.valor);
-	 		div1.appendChild(text);
-	 		div.appendChild(div1);
-	 		datos.appendChild(div);
-	 		div2.appendChild(text2);
-	 		div.appendChild(div2);
-	 		datos.appendChild(div);
-			}else
-			{
-				document.getElementById("datos").innerHTML="No has introducido tu nombre y tu password";
-			}
+	mostrarComentarios();
+}
+
+function mostrarComentarios () {
+	document.createElement('DIV').innerHTML = '';
+
+	for (var clave in localStorage){
+		 var comentario = localStorage[clave];
+
+		var div = document.createElement('DIV');
+		div.setAttribute("class", "comentarios");
+		var div1 = document.createElement('DIV');
+		div1.setAttribute("class", "div1");
+		var div2 = document.createElement('DIV');
+		div2.setAttribute("class", "div2");
+		var datos = document.getElementById('datos');
+		var text= document.createTextNode( clave);
+		var text2= document.createTextNode( comentario );
+		div1.appendChild(text);
+		div.appendChild(div1);
+		datos.appendChild(div);
+		div2.appendChild(text2);
+		div.appendChild(div2);
+		datos.appendChild(div);
+
+
+	}
+
+
 }
 // function guardarDatos(){
 // 	var clave = document.getElementById("clave").value;
